b5b0951aa26a0e0817d82c61e3d490bacd37dbed
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-06-20 18:52:07 -0600

    adding a method to get User by id in user services

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -2,13 +2,17 @@ import UserModel from "../models/UserModel";
 import { GenericCrudService } from "./GenericCrudService";
 import { userData } from "../data/UserData";
 
-export default class UserService{
-    private userService: GenericCrudService<UserModel>;
-    constructor(){
-        this.userService = new GenericCrudService<UserModel>(userData);
-    }
+export default class UserService {
+  private userService: GenericCrudService<UserModel>;
+  constructor() {
+    this.userService = new GenericCrudService<UserModel>(userData);
+  }
 
-    getAllUsers(): UserModel[] {
-        return this.userService.getAll();
-    }
+  getAllUsers(): UserModel[] {
+    return this.userService.getAll();
+  }
+
+  getUserById(id: string) {
+    return this.userService.getById(id);
+  }
 }
