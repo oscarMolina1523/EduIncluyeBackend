81f5d5713db919057561289f0289712a2ad587ea
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-06-30 12:36:00 -0600

    creating data for category

---
+++ src/data/CategoryData.ts
@@ -0,0 +1,8 @@
+import CategoryModel from "../models/CategoryModel";
+
+export let categoryData : CategoryModel[];
+
+categoryData=[
+    new CategoryModel("Señas", "señas mas comunes", "youtube.com", "image.png", true),
+    new CategoryModel("Familia", "apodos comunes de familia", "youtube.com", "image.png", true),
+];
