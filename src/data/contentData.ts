2811adb3d3212fb63a590f956d0ea7fdc7864580
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-06-30 14:00:01 -0600

    creating data for content

---
+++ src/data/contentData.ts
@@ -0,0 +1,8 @@
+import ContentModel from "../models/ContentModel";
+
+export let contentData: ContentModel[];
+
+contentData=[
+    new ContentModel("hVUGF67FYFHJ", "Mama", "aprende a decir mama", "youtube.com", "audio.com", true, "gjvjY88YGh"),
+    new ContentModel("hVUGsrbeFYFHJ", "Hola", "aprende a saludar", "youtube.com", "audio.com", true, "gjvjvfGU866"),
+]
