03425cab36b805a8bbe76b48edfd0ae626da1c9d
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-07-01 12:19:50 -0600

    creating data for graduates

---
+++ src/data/GraduatesData.ts
@@ -0,0 +1,8 @@
+import GraduatesModel from "../models/Graduates";
+
+export let graduatesData: GraduatesModel[];
+
+graduatesData=[
+    new GraduatesModel("jgfvj7TF7YVTU", "Jorge Lopes", "discapacidad auditiva", "image.png"),
+    new GraduatesModel("jgfviygh7YVTU", "Roberto Gonsales", "discapacidad del habla", "image.png"),
+]
