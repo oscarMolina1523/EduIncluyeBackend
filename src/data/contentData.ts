b9d85a9f677d7642ae82a0711692a3ba1d46e281
Oscar Danilo Molina <147948173+oscarMolina1523@users.noreply.github.com>
2025-07-05 13:40:54 -0600

    Update contentData.ts

--- src/data/contentData.ts
+++ src/data/contentData.ts
@@ -3,6 +3,6 @@ import ContentModel from "../models/ContentModel";
 export let contentData: ContentModel[];
 
 contentData=[
-    new ContentModel("hVUGF67FYFHJ", "Mama", "aprende a decir mama", "youtube.com", "audio.com", true, "gjvjY88YGh"),
+    new ContentModel("hVUGF67FYFHJ", "Facil", "aprende cada dia de la semana", "https://youtube.com/shorts/umwNSuLF38k?si=qpcm6u1_4SOrgPD7", "audio.com", true, "gjvjY88YGh"),
     new ContentModel("hVUGsrbeFYFHJ", "Hola", "aprende a saludar", "youtube.com", "audio.com", true, "gjvjvfGU866"),
 ]
