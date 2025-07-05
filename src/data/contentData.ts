a19ad4fb19611f7a33138a4c3b4b1eed76525c64
Oscar Danilo Molina <147948173+oscarMolina1523@users.noreply.github.com>
2025-07-05 13:45:26 -0600

    Update contentData.ts
    adding colors

--- src/data/contentData.ts
+++ src/data/contentData.ts
@@ -5,4 +5,5 @@ export let contentData: ContentModel[];
 contentData=[
     new ContentModel("hVUGF67FYFHJ", "Facil", "aprende cada dia de la semana", "https://youtube.com/shorts/umwNSuLF38k?si=qpcm6u1_4SOrgPD7", "audio.com", true, "gjvjvfGU866"),
     new ContentModel("hVUGsrbeFYFHJ", "Facil", "aprende los numeros enteros", "https://youtube.com/shorts/ol64bxm1qx8?si=_WXnNumhOTn8vMW5", "audio.com", true, "gjvjY88YGh"),
+    new ContentModel("hVUGsrbehbobFYFHJ", "Facil", "aprende los colores primarios", "https://youtube.com/shorts/VAR7HlOSdzk?si=hxL-UxjrnEMZONxC", "audio.com", true, "gjvjgjGh"),
 ]
