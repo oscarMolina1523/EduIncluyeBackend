f1d59cead5967edb3316da1311b3843cde55a1e3
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-07-06 14:18:09 -0600

    adding image of random people

--- src/data/GraduatesData.ts
+++ src/data/GraduatesData.ts
@@ -3,6 +3,6 @@ import GraduatesModel from "../models/Graduates";
 export let graduatesData: GraduatesModel[];
 
 graduatesData=[
-    new GraduatesModel("jgfvj7TF7YVTU", "Jorge Lopes", "discapacidad auditiva", "image.png"),
-    new GraduatesModel("jgfviygh7YVTU", "Roberto Gonsales", "discapacidad del habla", "image.png"),
+    new GraduatesModel("jgfvj7TF7YVTU", "Jorge Lopes", "discapacidad auditiva", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_vVBAvcz_VzzBC-8kmKTJ6j3B7t3LbYOhhg&s"),
+    new GraduatesModel("jgfviygh7YVTU", "Roberto Gonsales", "discapacidad del habla", "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fHww"),
 ]
