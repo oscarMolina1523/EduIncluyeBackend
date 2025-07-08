70b5c4f454c9a3c1a85cc420a13de1fc12f8cf28
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-07-08 13:51:54 -0600

    changing the description of categories

--- src/data/CategoryData.ts
+++ src/data/CategoryData.ts
@@ -6,23 +6,23 @@ categoryData = [
   new CategoryModel(
     "gjvjvfGU866",
     "Dias de la semana",
-    "como decir de manera correcta cada dia de la semana",
+    "Aprenda a señalar correctamente los días de la semana.",
     "https://youtube.com/shorts/wVuDj9V3olo?feature=share",
     "image.png",
     true
   ),
   new CategoryModel(
     "gjvjY88YGh",
-    "Numeros enteros",
-    "como decir cada numero y aprender a contar",
+    "Numeros",
+    "Conoce los números de una manera interactiva y emocionante.",
     "https://youtube.com/shorts/SOrseU8r04o?feature=share",
     "image.png",
     true
   ),
   new CategoryModel(
     "gjvjgjGh",
-    "Colores primarios",
-    "como decir cada uno de los colores primarios",
+    "Colores",
+    "Aprenda los diferentes tipos de colores.",
     "https://youtube.com/shorts/HYHEGcQD3x4?feature=share",
     "image.png",
     true
@@ -30,7 +30,7 @@ categoryData = [
   new CategoryModel(
     "gjvjgjG976h",
     "Comidas",
-    "aprende el nombre de cada comida",
+    "Aprenda cómo referirse a alimentos y bebidas comunes.",
     "https://youtube.com/shorts/c9e0YoHgzLE",
     "image.png",
     true
@@ -38,7 +38,7 @@ categoryData = [
   new CategoryModel(
     "gjvjhvlkh976h",
     "Saludos",
-    "aprende a saludar de manera correcta",
+    "Aprenda los diferentes tipos de Saludos.",
     "https://youtube.com/shorts/uDkq77eatok?feature=share",
     "image.png",
     true
@@ -46,7 +46,7 @@ categoryData = [
   new CategoryModel(
     "gjvjhvlkYFH6h",
     "Familia",
-    "aprende el nombre de los familiares",
+    "En esta sección aprenderás a decir los nombres de los miembros de la familia en Lengua de Señas",
     "https://youtube.com/shorts/JtfSS8Gtw8s?feature=share",
     "image.png",
     true
@@ -54,15 +54,15 @@ categoryData = [
   new CategoryModel(
     "gjvjhvkhbkH6h",
     "Señas",
-    "aprende lo mejor del mejor",
+    "Esta categoría incluye subcategorías como Familia, Saludos, Colores y Números, diseñadas para ayudarte a comunicarte de manera inclusiva y efectiva desde lo más básico hasta lo cotidiano.",
     "https://youtube.com/shorts/ltAopKpKJts?feature=share",
     "image.png",
     true
   ),
   new CategoryModel(
     "gjjhvjvkhbkH6h",
-    "Alfabeto",
-    "aprende cada letra del abecedario",
+    "Alfabeto Manual",
+    "La presente categoría pretende enseñar el alfabeto manual del Lenguaje de Señas, facilitando el aprendizaje de cada letra a través de señas claras. ",
     "https://youtube.com/shorts/efVI5k6hkvk?feature=share",
     "image.png",
     true
