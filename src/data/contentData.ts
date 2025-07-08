3cf19ab5c699bc9fcfe5603d1b6e1f4c6da49d7f
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-07-08 13:58:27 -0600

    adding data for family

--- src/data/contentData.ts
+++ src/data/contentData.ts
@@ -2,18 +2,198 @@ import ContentModel from "../models/ContentModel";
 
 export let contentData: ContentModel[];
 
-contentData=[
-    new ContentModel("hVUGF67FYFHJ", "Facil", "aprende cada dia de la semana", "https://youtube.com/shorts/umwNSuLF38k?si=qpcm6u1_4SOrgPD7", "audio.com", true, "gjvjvfGU866"),
-    new ContentModel("hVUGsrbeFYFHJ", "Facil", "aprende los numeros enteros", "https://youtube.com/shorts/ol64bxm1qx8?si=_WXnNumhOTn8vMW5", "audio.com", true, "gjvjY88YGh"),
-    new ContentModel("hVUGsrbehbobFYFHJ", "Facil", "aprende los colores primarios", "https://youtube.com/shorts/VAR7HlOSdzk?si=hxL-UxjrnEMZONxC", "audio.com", true, "gjvjgjGh"),
-    new ContentModel("hVUjhvjehbobFj7FHJ", "Facil", "aprende las principales comidas", "https://youtube.com/shorts/Q1jgvGFRo5o?si=ScNPk1uqhasYDDkQ", "audio.com", true, "gjvjgjG976h"),
-    new ContentModel("hVUGsrbjgvjv76tFYFHJ", "Intermedio", "aprende las principales comidas", "https://youtube.com/shorts/e7bItJRGfgU?si=i4hnYiX-rJWa9qb1", "audio.com", true, "gjvjgjG976h"),
-    new ContentModel("hVUGsrbjggvFHJ", "Facil", "aprende a decir HOLA", "https://youtube.com/shorts/JPJ5oHsExdc?si=TAzLSWFIf4TCeX9y", "audio.com", true, "gjvjhvlkh976h"),
-    new ContentModel("hVUGsrbjggv56rfJ", "Facil", "aprende a decir BUENOS DIAS", "https://youtube.com/shorts/UClL8_YjsKU?si=2QRs1xTgeCFudmGc", "audio.com", true, "gjvjhvlkh976h"),
-    new ContentModel("hVUGsrbgfc6rfJ", "Facil", "aprende a decir FAMILIA", "https://youtube.com/shorts/VDulhDu51aM?si=FLQkLOdJFXBvjNrf", "audio.com", true, "gjvjhvlkYFH6h"),
-    new ContentModel("hVUGsrbgfcjgvjJ", "Facil", "aprende a decir MAMA", "https://youtube.com/shorts/BEeZVMqdcaQ?si=Le2iAGxcKhNf89xU", "audio.com", true, "gjvjhvlkYFH6h"),
-    new ContentModel("hVUGsrbgugvjvgvjJ", "Facil", "aprende a decir HERMANO", "https://youtube.com/shorts/v3s3bsZQVOs?si=HhX2wDmkuqyhPbxo", "audio.com", true, "gjvjhvlkYFH6h"),
-    new ContentModel("hVUGsrbgjJ", "Facil", "aprende emociones", "https://youtube.com/shorts/C3WGhLX7J2k?si=B9fPL_Tc0Tk5fUtc", "audio.com", true, "gjvjhvkhbkH6h"),
-    new ContentModel("hVUGsrbgihybobjJ", "Facil", "aprende frases basicas", "https://youtube.com/shorts/72kThwEHfqY?si=h1wXqX6OTOsZT_6b", "audio.com", true, "gjvjhvkhbkH6h"),
-    new ContentModel("hVUGsihbk7bjJ", "Facil", "aprende el alfabeto", "https://youtube.com/shorts/nqOxrfv6Q8A?si=L56n3P7M50Wx_KMQ", "audio.com", true, "gjjhvjvkhbkH6h"),
-]
+contentData = [
+  new ContentModel(
+    "hVUGF67FYFHJ",
+    "Facil",
+    "aprende cada dia de la semana",
+    "https://youtube.com/shorts/umwNSuLF38k?si=qpcm6u1_4SOrgPD7",
+    "audio.com",
+    true,
+    "gjvjvfGU866"
+  ),
+  new ContentModel(
+    "hVUGsrbeFYFHJ",
+    "Facil",
+    "aprende los numeros enteros",
+    "https://youtube.com/shorts/ol64bxm1qx8?si=_WXnNumhOTn8vMW5",
+    "audio.com",
+    true,
+    "gjvjY88YGh"
+  ),
+  //colores
+  new ContentModel(
+    "hVUGsrbehbobFYFHJ",
+    "Rojo",
+    "aprende los colores primarios",
+    "https://youtube.com/shorts/VAR7HlOSdzk?si=hxL-UxjrnEMZONxC",
+    "audio.com",
+    true,
+    "gjvjgjGh"
+  ),
+  new ContentModel(
+    "hVUGsrbehbobFYFHJ",
+    "Verde",
+    "aprende los colores primarios",
+    "https://youtube.com/shorts/VAR7HlOSdzk?si=hxL-UxjrnEMZONxC",
+    "audio.com",
+    true,
+    "gjvjgjGh"
+  ),
+  new ContentModel(
+    "hVUGsrbehbobFYFHJ",
+    "Amarillo",
+    "aprende los colores primarios",
+    "https://youtube.com/shorts/VAR7HlOSdzk?si=hxL-UxjrnEMZONxC",
+    "audio.com",
+    true,
+    "gjvjgjGh"
+  ),
+  new ContentModel(
+    "hVUGsrbehbobFYFHJ",
+    "Naranja",
+    "aprende los colores primarios",
+    "https://youtube.com/shorts/VAR7HlOSdzk?si=hxL-UxjrnEMZONxC",
+    "audio.com",
+    true,
+    "gjvjgjGh"
+  ),
+  new ContentModel(
+    "hVUGsrbehbobFYFHJ",
+    "Azul",
+    "aprende los colores primarios",
+    "https://youtube.com/shorts/VAR7HlOSdzk?si=hxL-UxjrnEMZONxC",
+    "audio.com",
+    true,
+    "gjvjgjGh"
+  ),
+  new ContentModel(
+    "hVUGsrbehbobFYFHJ",
+    "Blanco",
+    "aprende los colores primarios",
+    "https://youtube.com/shorts/VAR7HlOSdzk?si=hxL-UxjrnEMZONxC",
+    "audio.com",
+    true,
+    "gjvjgjGh"
+  ),
+  //comidas
+  new ContentModel(
+    "hVUjhvjehbobFj7FHJ",
+    "Facil",
+    "aprende las principales comidas",
+    "https://youtube.com/shorts/Q1jgvGFRo5o?si=ScNPk1uqhasYDDkQ",
+    "audio.com",
+    true,
+    "gjvjgjG976h"
+  ),
+  new ContentModel(
+    "hVUGsrbjgvjv76tFYFHJ",
+    "Intermedio",
+    "aprende las principales comidas",
+    "https://youtube.com/shorts/e7bItJRGfgU?si=i4hnYiX-rJWa9qb1",
+    "audio.com",
+    true,
+    "gjvjgjG976h"
+  ),
+  new ContentModel(
+    "hVUGsrbjggvFHJ",
+    "Facil",
+    "aprende a decir HOLA",
+    "https://youtube.com/shorts/JPJ5oHsExdc?si=TAzLSWFIf4TCeX9y",
+    "audio.com",
+    true,
+    "gjvjhvlkh976h"
+  ),
+  new ContentModel(
+    "hVUGsrbjggv56rfJ",
+    "Facil",
+    "aprende a decir BUENOS DIAS",
+    "https://youtube.com/shorts/UClL8_YjsKU?si=2QRs1xTgeCFudmGc",
+    "audio.com",
+    true,
+    "gjvjhvlkh976h"
+  ),
+  //Familia
+  new ContentModel(
+    "hVUGsrbgfc6rfJ",
+    "Mamá",
+    "aprende a decir FAMILIA",
+    "https://youtube.com/shorts/VDulhDu51aM?si=FLQkLOdJFXBvjNrf",
+    "audio.com",
+    true,
+    "gjvjhvlkYFH6h"
+  ),
+  new ContentModel(
+    "hVUGsrbgfc6rfJ",
+    "Papá",
+    "aprende a decir FAMILIA",
+    "https://youtube.com/shorts/VDulhDu51aM?si=FLQkLOdJFXBvjNrf",
+    "audio.com",
+    true,
+    "gjvjhvlkYFH6h"
+  ),
+  new ContentModel(
+    "hVUGsrbgfc6rfJ",
+    "Hermano",
+    "aprende a decir FAMILIA",
+    "https://youtube.com/shorts/VDulhDu51aM?si=FLQkLOdJFXBvjNrf",
+    "audio.com",
+    true,
+    "gjvjhvlkYFH6h"
+  ),
+  new ContentModel(
+    "hVUGsrbgfc6rfJ",
+    "Hermana",
+    "aprende a decir FAMILIA",
+    "https://youtube.com/shorts/VDulhDu51aM?si=FLQkLOdJFXBvjNrf",
+    "audio.com",
+    true,
+    "gjvjhvlkYFH6h"
+  ),
+  new ContentModel(
+    "hVUGsrbgfc6rfJ",
+    "Abuelo",
+    "aprende a decir FAMILIA",
+    "https://youtube.com/shorts/VDulhDu51aM?si=FLQkLOdJFXBvjNrf",
+    "audio.com",
+    true,
+    "gjvjhvlkYFH6h"
+  ),
+  new ContentModel(
+    "hVUGsrbgfc6rfJ",
+    "Abuela",
+    "aprende a decir FAMILIA",
+    "https://youtube.com/shorts/VDulhDu51aM?si=FLQkLOdJFXBvjNrf",
+    "audio.com",
+    true,
+    "gjvjhvlkYFH6h"
+  ),
+  //emociones
+  new ContentModel(
+    "hVUGsrbgjJ",
+    "Facil",
+    "aprende emociones",
+    "https://youtube.com/shorts/C3WGhLX7J2k?si=B9fPL_Tc0Tk5fUtc",
+    "audio.com",
+    true,
+    "gjvjhvkhbkH6h"
+  ),
+  new ContentModel(
+    "hVUGsrbgihybobjJ",
+    "Facil",
+    "aprende frases basicas",
+    "https://youtube.com/shorts/72kThwEHfqY?si=h1wXqX6OTOsZT_6b",
+    "audio.com",
+    true,
+    "gjvjhvkhbkH6h"
+  ),
+  new ContentModel(
+    "hVUGsihbk7bjJ",
+    "Facil",
+    "aprende el alfabeto",
+    "https://youtube.com/shorts/nqOxrfv6Q8A?si=L56n3P7M50Wx_KMQ",
+    "audio.com",
+    true,
+    "gjjhvjvkhbkH6h"
+  ),
+];
