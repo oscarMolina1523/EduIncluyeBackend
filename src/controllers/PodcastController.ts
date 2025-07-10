7e0312cfa6ed7a907d1e4a322e726e09ce90fdce
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-07-10 16:23:06 -0600

    implementing correctly service of podcast updated

--- src/controllers/PodcastController.ts
+++ src/controllers/PodcastController.ts
@@ -10,60 +10,62 @@ export default class PodcastController {
     this.service = new PodcastService();
   }
 
-  getAllPodcasts = (req: Request, res: Response) => {
-    const podcasts = this.service.getAllPodcasts();
-    res.status(200).json(podcasts);
+  getAllPodcasts = async (req: Request, res: Response) => {
+    try{
+      const podcasts = await this.service.getAllPodcasts();
+      res.status(200).json(podcasts);
+    }catch(error){
+      console.error(error);
+      res.status(500).json({message: "Failed to get podcast data"});
+    }
   };
 
-  getPodcastById = (req: Request, res: Response) => {
+  getPodcastById = async (req: Request, res: Response) => {
     const podcastId = req.params.id;
-    const podcast = this.service.getPodcastById(podcastId);
 
-    if (podcast) {
+    try{
+      const podcast = await this.service.getPodcastById(podcastId);
       res.status(200).json(podcast);
-    } else {
+    }catch(error){
+      console.error(error);
       res.status(404).json({ message: "Podcast not found" });
     }
   };
 
-  addPodcast = (req: Request, res: Response) => {
+  addPodcast = async (req: Request, res: Response) => {
     const { name, description, video, audio, isActive } = req.body;
-    const id = generateId();
 
-    // ✅ Crear instancia de PodcastModel para generar el id
-    const newPodcast = new PodcastModel(id, name, description, video, audio, isActive);
+    const newPodcast = {name, description, video, audio, isActive};
+    try{
+      const createdPodcast = await this.service.addPodcast(newPodcast);
+      res.status(201).json({ message: "Podcast added correctly", podcast: createdPodcast });
 
-    const result = this.service.addPodcast(newPodcast);
-
-    if (result) {
-      res.status(201).json({ message: "Podcast added correctly" });
-    } else {
+    }catch(error){
+      console.error(error);
       res.status(400).json({ message: "Failed to add the podcast" });
     }
   };
 
-  updatePodcast = (req: Request, res: Response) => {
+  updatePodcast = async (req: Request, res: Response) => {
     const id = req.params.id;
     const data = req.body;
-
-    const result = this.service.updatePodcast(id, data);
-
-    if (result.success) {
-      res.status(200).json({ message: result.message });
-    } else {
-      res.status(404).json({ message: result.message });
+    try{
+      await this.service.updatePodcast(id, data);
+      res.status(200).json({ message: "Podcast updated correctly" });
+    }catch(error){
+      console.error(error);
+      res.status(404).json({ message: "result.message" });
     }
   };
 
-  deletePodcast = (req: Request, res: Response) => {
+  deletePodcast = async (req: Request, res: Response) => {
     const id = req.params.id;
-
-    const result = this.service.deletePodcast(id);
-
-    if (result.success) {
-      res.status(200).json({ message: result.message });
-    } else {
-      res.status(404).json({ message: result.message });
+    try{
+      await this.service.deletePodcast(id);
+      res.status(200).json({ message: "Podcast deleted" });
+    }catch(error){
+      console.error(error);
+      res.status(404).json({ message: "Failed to delete the podcast" });
     }
   };
 }
