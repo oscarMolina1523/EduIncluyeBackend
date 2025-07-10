36c11b900e07ee244c3f1df96391e65233d2d83c
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-07-10 16:05:17 -0600

    implementing service of firestore correctly in graduates controller

--- src/controllers/GraduatesController.ts
+++ src/controllers/GraduatesController.ts
@@ -10,60 +10,65 @@ export default class GraduatesController {
     this.service = new GraduatesService();
   }
 
-  getAllGraduates = (req: Request, res: Response) => {
-    const graduates = this.service.getAllGraduates();
-    res.status(200).json(graduates);
+  getAllGraduates = async (req: Request, res: Response) => {
+    try {
+      const graduates = await this.service.getAllGraduates();
+      res.status(200).json(graduates);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Failed to get graduates" });
+    }
   };
 
-  getGraduateById = (req: Request, res: Response) => {
+  getGraduateById = async (req: Request, res: Response) => {
     const graduateId = req.params.id;
-    const graduate = this.service.getGraduateById(graduateId);
-
-    if (graduate) {
+    try {
+      const graduate = await this.service.getGraduateById(graduateId);
       res.status(200).json(graduate);
-    } else {
+    } catch (error) {
+      console.error(error);
       res.status(404).json({ message: "Graduate not found" });
     }
   };
 
-  addGraduate = (req: Request, res: Response) => {
+  addGraduate = async (req: Request, res: Response) => {
     const { name, description, image } = req.body;
-    const id = generateId();
-
-    // ✅ Crear instancia de GraduatesModel para generar el id
-    const newGraduate = new GraduatesModel(id, name, description, image);
-
-    const result = this.service.addGraduate(newGraduate);
-
-    if (result) {
-      res.status(201).json({ message: "Graduate added correctly" });
-    } else {
+    const newGraduate = { name, description, image };
+    try {
+      const createdGraduate = await this.service.addGraduate(newGraduate);
+      res
+        .status(201)
+        .json({
+          message: "Graduate added correctly",
+          graduate: createdGraduate,
+        });
+    } catch (error) {
+      console.error(error);
       res.status(400).json({ message: "Failed to add the graduate" });
     }
   };
 
-  updateGraduate = (req: Request, res: Response) => {
+  updateGraduate = async (req: Request, res: Response) => {
     const id = req.params.id;
     const data = req.body;
-
-    const result = this.service.updateGraduate(id, data);
-
-    if (result.success) {
-      res.status(200).json({ message: result.message });
-    } else {
-      res.status(404).json({ message: result.message });
+    try {
+      await this.service.updateGraduate(id, data);
+      res.status(200).json({ message: "Graduate updated" });
+    } catch (error) {
+      console.error(error);
+      res.status(404).json({ message: "Failed to update graduated"});
     }
   };
 
-  deleteGraduate = (req: Request, res: Response) => {
+  deleteGraduate = async (req: Request, res: Response) => {
     const id = req.params.id;
+    try{
+      await this.service.deleteGraduate(id);
 
-    const result = this.service.deleteGraduate(id);
-
-    if (result.success) {
-      res.status(200).json({ message: result.message });
-    } else {
-      res.status(404).json({ message: result.message });
+      res.status(200).json({ message: "Graduate deleted" });
+    }catch(error){
+      console.error(error);
+      res.status(404).json({ message: "Failed to delete Graduate" });
     }
   };
 }
